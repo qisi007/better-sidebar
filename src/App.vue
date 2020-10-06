feb81e681c2e462eab12d929e7868829c75b84ff
QJ刘贵生 <codeliuguisheng@163.com>
2020-10-06 14:07:06 +0800

    add examples

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div id="app" style="height: 2000px">
+  <div id="app" style="height: 2000px;">
+    <div style="height: 800px;background: pink"></div>
+    <div style="height: 800px;background: yellow"></div>
+    <div style="height: 800px;background: #ccc"></div>
     <better-sidebar :topButton=topButton>
       <better-sidebar-item
         class="cell-box"
         icon="icon-gonggao iconfont"
         title="公告"
         popver="true"
-        >插槽</better-sidebar-item
+        ><img src="https://img-blog.csdnimg.cn/20191201090129350.jpg" alt=""></better-sidebar-item
       >
       <better-sidebar-item
         class="cell-box"
@@ -18,6 +21,18 @@
         class="cell-box"
         icon="icon-huodong iconfont"
         title="活动"
+      />
+      <better-sidebar-item
+        class="cell-box"
+        icon="icon-fuli iconfont"
+        title="来源"
+        link="https://www.allqj.com"
+      />
+      <better-sidebar-item
+        class="cell-box"
+        icon="icon-yijianfankui iconfont"
+        title="意见反馈"
+        link="https://www.allqj.com"
       />
     </better-sidebar>
   </div>
